e5b5e809f7bc5518fc8b426b533fdc5b5ec842b3
David Irvine <david.irvine@maidsafe.net>
2016-12-22 17:40:00 +0000

    a test \nSome text \n- [fixed] A sample fix.

--- src/gene.rs
+++ src/gene.rs
@@ -1,5 +1,6 @@
 use link::Link;
 use network_node::NetworkNode;
+use std::cell::RefCell;
 use std::rc::Rc;
 use std::sync::RwLock;
 use traits::Traits;
@@ -8,7 +9,7 @@ use weight::Weight;
 /// A gene
 #[derive(PartialEq, Clone)]
 pub struct Gene {
-    lnk: Rc<Link>,
+    lnk: RefCell<Link>,
     innovation_num: u64,
     mutation_amount: f64, // Used to see how much mutation has changed the link
     enabled: bool, // When this is off the Gene is disabled
@@ -17,8 +18,8 @@ pub struct Gene {
 
 impl Gene {
     /// Construct a gene
-    pub fn new_no_trait(onode: Rc<NetworkNode>,
-                        inode: Rc<NetworkNode>,
+    pub fn new_no_trait(onode: RefCell<NetworkNode>,
+                        inode: RefCell<NetworkNode>,
                         link_trait: Traits,
                         link_weight: Weight,
                         innov: u64,
@@ -26,7 +27,7 @@ impl Gene {
                         recur: bool)
                         -> Gene {
         Gene {
-            lnk: Rc::new(Link::new(onode, inode, link_trait, link_weight, innov, recur)),
+            lnk: RefCell::new(Link::new(onode, inode, link_trait, link_weight, innov, recur)),
             innovation_num: innov,
             mutation_amount: mnum,
             enabled: true,
@@ -35,19 +36,19 @@ impl Gene {
     }
 
     /// Construct a gene off of another gene as a duplicate
-    pub fn new_use_existing_gene(onode: Rc<NetworkNode>,
-                                 inode: Rc<NetworkNode>,
+    pub fn new_use_existing_gene(onode: RefCell<NetworkNode>,
+                                 inode: RefCell<NetworkNode>,
                                  gene: &Gene,
                                  link_trait: Traits,
                                  recur: bool)
                                  -> Gene {
         Gene {
-            lnk: Rc::new(Link::new(onode,
-                                   inode,
-                                   link_trait,
-                                   gene.lnk.weight(),
-                                   gene.innovation_num(),
-                                   recur)),
+            lnk: RefCell::new(Link::new(onode,
+                                        inode,
+                                        link_trait,
+                                        gene.lnk.borrow().weight(),
+                                        gene.innovation_num(),
+                                        recur)),
             innovation_num: gene.innovation_num(),
             mutation_amount: gene.mutation_amount(),
             enabled: gene.enabled(),
@@ -55,6 +56,11 @@ impl Gene {
         }
     }
 
+    /// Set link trait
+    pub fn set_link_trait(&mut self, traits: Traits) {
+        unimplemented!()
+        // self.lnk.set_trait(traits);
+    }
     /// Getter
     pub fn innovation_num(&self) -> u64 {
         self.innovation_num
@@ -70,6 +76,10 @@ impl Gene {
     /// Getter
     pub fn frozen(&self) -> bool {
         self.frozen
+    }
+    /// Set gene frozen state
+    pub fn set_frozen(&mut self, frozen: bool) {
+        self.frozen = frozen;
     }
 
     // 	//Print gene to a file- called from Genome
