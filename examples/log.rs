f3c68594d2d4fc793b5c44cf4e301f2a3590a715
David Irvine <david.irvine@maidsafe.net>
2017-07-31 21:06:03 +0100

    Strucured logging

--- examples/log.rs
+++ examples/log.rs
@@ -1,12 +1,13 @@
-#[macro_use]
-extern crate slog;
-extern crate slog_term;
-
-use slog::DrainExt;
-
-fn main() {
-    let drain = slog_term::streamer().build().fuse();
-    let root_logger = slog::Logger::root(drain, o!("version" => "0.5"));
-    info!(root_logger, "Evolution(s) started";
-        "started_at" => format!("{}", time::now().rfc3339()));
-}
+// #[macro_use]
+// extern crate slog;
+// extern crate slog_term;
+//
+// use slog::DrainExt;
+//
+// fn main() {
+//     let drain = slog_term::streamer().build().fuse();
+//     let root_logger = slog::Logger::root(drain, o!("version" => "0.5"));
+//     info!(root_logger, "Evolution(s) started";
+//         "started_at" => format!("{}", time::now().rfc3339()));
+// }
+fn main() {}
--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 //! (http://nn.cs.utexas.edu/downloads/papers/stanley.ec02.pdf)
 //! This implementation is a playground for the moment.
 
-#[macro_use]
+// #[macro_use]
 extern crate log;
 extern crate rand;
 extern crate rulinalg;
@@ -15,57 +15,39 @@ extern crate num_cpus;
 extern crate crossbeam;
 
 
-mod genome;
-/// A gene holds a `Link`. It alse records the mutation quantity of the `Link` and whether it is
-/// frozen (no longer able to be mutated) or enabled/disabled
-pub mod gene;
-/// A link connects 2 `NetworkNodes` and has a `Weight` associated with it.
-pub mod link;
-/// A `NetworkNode` can be an Input (Sensor), Output or Hidden.
-pub mod network_node;
-mod innovation_database;
-mod innovation;
-mod organism;
-mod species;
-mod population;
-mod parameters;
-mod selection;
-mod traits;
-mod network;
-mod novelty_item;
-mod weight;
-
-
 use rand::distributions::{IndependentSample, Range};
 use rand::distributions::normal::StandardNormal;
 
-fn rand_float() -> f64 {
+/// TODO - should not be public
+pub fn rand_float() -> f64 {
     let mut rng = rand::thread_rng();
     Range::new(-1f64, 1.).ind_sample(&mut rng)
 }
 
-fn gaussrand() -> f64 {
+/// TODO - should not be public
+pub fn gaussrand() -> f64 {
     let StandardNormal(x) = rand::random();
     x
 }
 
-fn sigmoid(activesum: f64, slope: f64) -> f64 {
+/// TODO - should not be public
+pub fn sigmoid(activesum: f64, slope: f64) -> f64 {
     1.0 / (1.0 + (-(slope * activesum).exp())) //Compressed
 }
 
-fn hebbian(weight: f64,
-           maxweight: f64,
-           active_in: f64,
-           active_out: f64,
-           hebb_rate: f64,
-           pre_rate: f64,
-           post_rate: f64)
-           -> f64 {
+/// TODO - should not be public
+pub fn hebbian(weight: f64,
+               maxweight: f64,
+               active_in: f64,
+               active_out: f64,
+               hebb_rate: f64,
+               pre_rate: f64)
+               -> f64 {
 
     let mut neg = false;
-    let mut delta: f64;
+    let delta: f64;
 
-    let mweight = if maxweight < 5.0 { 5.0 } else { maxweight };
+    //let mweight = if maxweight < 5.0 { 5.0 } else { maxweight };
 
     let w = if weight > maxweight {
         maxweight
@@ -123,3 +105,9 @@ fn hebbian(weight: f64,
 // axon links neurons and synapses connect them (firing signals)
 //
 // axons also link lobes (cortex) (species) together.
+//
+mod tests {
+    #[test]
+    fn it_works() {}
+
+}
